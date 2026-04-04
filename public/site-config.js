9b4d190a2808acf47fd8f4a1abfeadec27482e73
yokel99 <yokel.99@gmail.com>
2026-04-04 20:48:38 +0700

    Update site-config.js

--- public/site-config.js
+++ public/site-config.js
@@ -1,6 +1,6 @@
 const isLocal = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
 
-const SERVER_HOST = isLocal ? 'http://localhost:3005' : 'https://api.initcraft.com';
+const SERVER_HOST = isLocal ? 'http://localhost:3005' : 'http://122.155.197.183:3005';
 const API_PATH = '/api';
 const ASSETS_PATH = '/assets';
 const API_URL = SERVER_HOST + API_PATH;
